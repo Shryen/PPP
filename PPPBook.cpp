ae3cd995cbc16796b7d66209b29da13902a2cd40
Shryen <hbk9238@hotmail.com>
2025-09-11 19:05:58 +0200

    Simple use case of switch statement

--- PPPBook.cpp
+++ PPPBook.cpp
@@ -3,44 +3,20 @@
 
 int main()
 {
-    string FirstName = "";
-    cout << "What is your name? ";
-    cin >> FirstName;
-
-    string Target;
-    cout << "Hello " << FirstName << " who would you like to write for? ";
-    cin >> Target;
-
-    cout << "Dear " << Target << ", \n";
-
-    string FriendName = " ";
-    cout << "Name a friend: ";
-    cin >> FriendName;
-
-    char friend_sex = ' ';
-    cout << "Please give the friend's sex. M if male F if female: ";
-    cin >> friend_sex;
-
-    if (friend_sex == 'M')
-        cout << "If you see " << FriendName << " please ask him to call me!\n";
-    if (friend_sex == 'F')
-        cout << "If you see " << FriendName << " please ask her to call me!\n";
-
-    int age = 0;
-    cout << "Target's age: ";
-    cin >> age;
-    if (age <= 0 || age > 120) {
-        simple_error("You're kidding!");
+    int a{ 0 };
+    cout << "Please enter a number between 1-10!\n";
+    cin >> a;
+    switch (a) {
+    case 1: case 3: case 5: case 7: case 9:
+        cout << a << " is odd!";
+        break;
+    case 2: case 4: case 6: case 8: case 10:
+        cout << a << " is even!";
+        break;
+    default:
+        cout << a << " is not a digit or not between 1 and 10!";
+        break;
     }
-    cout << "I heard you just had your birthday and you are " << age << " years old!\n";
-    if (age <= 12)
-        cout << "Next year you will be " << age + 1 << "\n";
-    if (age == 17)
-        cout << "Next year you will be able to vote!\n";
-    if (age >= 70)
-        cout << "I hope you're enjoying your retirement!\n";
-
-    cout << "Yours sincerely, __ " << FirstName;
     
     cout << '\n';
     keep_window_open();
